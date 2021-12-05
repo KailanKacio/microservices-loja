59481b7c46a09b6391eeba990a4b7ee72084a779
Kailan <kailan.s.gabriel@gmail.com>
2021-12-05 11:20:03 -0300

    Addicionando micro serviço da loja

---
+++ src/main/java/br/com/microservice/loja/service/CompraService.java
@@ -0,0 +1,25 @@
+package br.com.microservice.loja.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.microservice.loja.client.FornecedorClient;
+import br.com.microservice.loja.controller.dto.CompraDTO;
+import br.com.microservice.loja.controller.dto.InfoFornecedorDTO;
+
+@Service
+public class CompraService {
+
+	@Autowired
+	private FornecedorClient fornecedorClient;
+
+	public void realizaCompra(CompraDTO compra) {
+
+		InfoFornecedorDTO info = fornecedorClient.getInfoPorEstado(
+				compra.getEndereco().getEstado());
+
+		System.out.println(info.getEndereco());
+
+	}
+
+}
